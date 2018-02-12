bee0156295b836ac8de2b3fcdd8a679b79b06a26
eashwer101 <manmeeth100@gmail.com>
2018-02-12 02:03:13 -0500

    Add files via upload

---
+++ mesosphere.repo.bash
@@ -0,0 +1,6 @@
+[mesosphere]
+name=Official mesosphere package repo
+baseurl=http://repos.mesosphere.io/el/7/$basearch/
+enabled=1
+gpgcheck=0
+gpgkey=https://repos.mesosphere.io/el/RPM-GPG-KEY-mesosphere
